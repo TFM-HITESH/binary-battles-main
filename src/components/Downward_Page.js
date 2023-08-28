3fe89586d4d6b7781c58f2ec04fae9856c9e11b3
TFM-HITESH <hiteshshivkumar@gmail.com>
2023-08-29 04:18:41 +0530

    Changes

--- src/components/Downward_Page.js
+++ src/components/Downward_Page.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
+
 function Downward_Page() {
   return (
     <div className='hero__header'>
-        <h1>John Doe</h1>
-        <h2>Frontend Developer</h2>
+        <h1 className='bg-cyan-500 p-10'>John Doe</h1>
+        <h2>Frontend Developer</h2> 
     </div>
   )
 }
 
 export default Downward_Page
+
+// h-100vh flex-col text-center items-center gap-2 justify-center

c8962134033c7e4db53d4876fa629f2a204fe167
TFM-HITESH <hiteshshivkumar@gmail.com>
2023-09-21 03:51:03 +0530

    Navbar Complete

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,39 +1,62 @@
+'use client';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import LoginDropdown from './LoginDropdown';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+import { faMagento } from '@fortawesome/free-brands-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const logoMain = "/src/app/Components/logo.png";
+
   return (
-    <nav className="bg-[#06081d] p-4 flex items-center justify-between">
+    <nav className="bg-[#06081d] p-4 flex items-center justify-between w-screen">
       {/* Logo */}
       <div className="flex items-center space-x-2">
-        <img src="/logo.png" alt="Logo" className="w-10 h-10" />
+        <Image
+          src={logoMain} // Replace with the actual path to your image
+          alt="Description of the image"
+          width={10} // Specify the width of the image
+          height={10} // Specify the height of the image
+        />
         <span className="text-white text-lg font-semibold"></span>
       </div>
 
       {/* Center Buttons */}
-      <div className="space-x-4 flex">
+      <div className="space-x-6 flex">
         <Link href="/Round1">
-          <div className="px-8 py-2 transition duration-300 ease-in-out opacity-50 transform hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100 animate-glow-opacity"> 
+          <div className="px-8 py-2 mr-3 ml-8 transition duration-300 ease-in-out opacity-50 transform hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100 animate-glow-opacity">
+            <FontAwesomeIcon icon={faPuzzlePiece} className='px-2 h-5 w-5'/> 
             Analytical Arena
           </div>
         </Link>
 
         <Link href="/Round2">
-          <div className="px-8 py-2 transition duration-300 ease-in-out opacity-50 transform hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100 animate-glow-opacity">
+          <div className="px-8 py-2 mr-3 ml-3 transition duration-300 ease-in-out opacity-50 transform hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100 animate-glow-opacity">
+          <FontAwesomeIcon icon={faMagento} className='px-2 h-5 w-5'/>
             Labyrinthine Odessey
           </div>
         </Link>
 
         <Link href="/Round3">
-          <div className="px-8 py-2 transition duration-300 ease-in-out opacity-50 transform hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100 animate-glow-opacity">
+          <div className="px-8 py-2 ml-3 transition duration-300 ease-in-out opacity-50 transform hover:bg-stone-300 hover:text-black hover:shadow-lg hover:scale-105 rounded-md hover:opacity-100 animate-glow-opacity">
+          <FontAwesomeIcon icon={faLock} className='px-2 h-5 w-5'/>
             The Enigma
           </div>
         </Link>
       </div>
 
       {/* Right Corner Login Button */}
-      <button className="bg-white text- hover:bg-blue-100 text-sm px-4 py-2 rounded-full">
+      {/* <button className="bg-cyan-600 w-[6%] hover:bg-cyan-900 text-sm px-4 py-2 rounded-full transition duration-300 ease-in-out">
         Login
-      </button>
+      </button> */}
+
+      <div className="p-4 ml-0">
+        <LoginDropdown />
+      </div>
     </nav>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
 import Image from 'next/image'
-import Navbar from './Components/navbar'
+import Navbar from './Components/Navbar'
 
 export default function Home() {
   return (
     <div className="">
-      <Navbar />
+      <div className='w-screen'>
+        <Navbar />
+      </div>
+      
+      <div className='bg-cyan-500 h-[90%]'>
+        hello
+      </div>
     </div>
+    
   )
 }
